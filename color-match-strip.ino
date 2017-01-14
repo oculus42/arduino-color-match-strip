6967b8d310b38db4686b01ecb6fc57974021269b
Samuel Rouse <oculus42@users.noreply.github.com>
2017-01-13 22:21:37 -0500

    Significant Rework

--- color-match-strip.ino
+++ color-match-strip.ino
@@ -1,67 +1,91 @@
 #include <Wire.h>
-
-// ********** Neopixel
-// Neopixel
-#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-#include <avr/power.h>
-#endif
-
-#define NEOPIN 6
-#define NEOCOUNT 9
-
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NEOCOUNT, NEOPIN, NEO_GRB + NEO_KHZ800);
-
-// ********** RGB Strip Controller - Next LED Strip Driver Deep Blue ARD0689
 #include "RGBdriver.h"
-#define CLK A3 //pins definitions for the driver        
-#define DIO A2
-
-RGBdriver Driver(CLK, DIO);
-
-// ********** RGB Sensor - Adafruit TCS34725
 #include "Adafruit_TCS34725.h"
+#include <IRLibRecvPCI.h>
+#include <IRLibDecodeBase.h>
+#include <IRLib_P01_NEC.h>
+#include <IRLibCombo.h>
 
-#define brightness 128
-
-Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
-
-// our RGB -> eye-recognized gamma color
-byte gammatable[256];
-
-// Simple struct for passing around colors
-struct color {
-  byte red;
-  byte green;
-  byte blue;
+// Structs and data for passing around colors
+struct color_s {
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
 };
 
-// Set the default at off.
-color stripColor = { 0, 0, 0 };
+struct irColor_s {
+  uint32_t key;
+  color_s color;
+};
 
-// Gamma Table code from the Adafruit_TCS34725 example
-void generateGammaTable() {
-  // thanks PhilB for this gamma table!
-  // it helps convert RGB colors to what humans see
-  for (int i = 0; i < 256; i++) {
-    float x = i;
-    x /= 255;
-    x = pow(x, 2.5);
-    x *= 255;
+irColor_s remoteColors [20] = {
+  {0xFF1AE5, {255, 0, 0} }, // Red
+  {0xFF2AD5, {255, 63, 0} }, // Red-Orange (Red 2)
+  {0xFF0AF5, {255, 127, 0} }, // Orange (Red 3)
+  {0xFF38C7, {255, 191, 0} }, // Yellow-Orange (Red 4)
+  {0xFF18E7, {255, 255, 0} }, // Yellow (Red 5)
+  {0xFF9A65, {0, 255, 0} }, // Green
+  {0xFFAA55, {0, 255, 63} }, // Green-Aqua (Green 2)
+  {0xFF8A75, {0, 255, 127} }, // Aqua (Green 3)
+  {0xFFB847, {0, 255, 191} }, // Aqua-Cyan (Green 4)
+  {0xFF9867, {0, 255, 255} }, // Cyan (Green 5)
+  {0xFFA25D, {0, 0, 255} }, // Blue
+  {0xFF926D, {63, 0, 255} }, // Blue-Purple (Blue 2)
+  {0xFFB24D, {127, 0, 255} }, // Purple (Blue 3)
+  {0xFF7887, {191, 0, 255} }, // Purple-Magenta (Blue 4)
+  {0xFF58A7, {255, 0, 255} }, // Magenta (Blue 5) 
+  {0xFF22DD, {255, 255, 255} }, // White
+  {0xFF12ED, {255, 191, 191} }, // White 2
+  {0xFF32CD, {255, 191, 191} }, // White 3
+  {0xFFF807, {191, 191, 255} }, // White 4
+  {0xFFD827, {191, 191, 255} } // White 5
+};
 
-    gammatable[i] = x;
-  }
-}
+
+// RGB Driver
+#define RGB_CLK A3        
+#define RGB_DATA A2
+
+RGBdriver Driver(RGB_CLK, RGB_DATA);
+
+// RGB Sensor
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+// IR Recevier
+#define IR_PIN 3
+
+#define RECV_BLINK_LEN 10
+
+IRrecvPCI myReceiver(IR_PIN); //pin number for the receiver
+IRdecode myDecoder;
+
+// ******** Globals
+
+bool poweredOn = true;
+bool getSensorData = true;
+
+uint8_t brightness = 127;
+
+color_s offColor = color_s {0, 0, 0};
+color_s currentColor = offColor;
+color_s nextColor = offColor;
+color_s storedColor;
+
+// Get Color from RGB Sensor
+uint16_t clear, red, green, blue;
+
+uint32_t sum;
+float r, g, b, offset;
+int16_t r1, g1, b1;
+
+uint8_t i, len;
+uint32_t currentInput;
+
+// ******** SETUP
 
 // Use the onboard LED as a failure indicator
 void setupWarningLight() {
   pinMode(LED_BUILTIN, OUTPUT);
-}
-
-// Should something go wrong, provide a way to tell
-void indicateError() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  while (1); // halt!
 }
 
 // Check for the sensor
@@ -72,13 +96,6 @@ void setupRGBSensor() {
   }
 }
 
-
-void setupNeoPixels() {
-  strip.begin();
-  // All off
-  strip.show();
-}
-
 // LED Strip can start in unexpected state so we must turn it off.
 void setupLEDStrip() {
   // Turn off strip
@@ -87,109 +104,187 @@ void setupLEDStrip() {
   Driver.end();
 }
 
-void setup() {
-  // Serial.begin(9600);
-  // Serial.println("Color Match");
-  setupWarningLight();
-  generateGammaTable();
-  setupNeoPixels();
-  setupLEDStrip();
-  setupRGBSensor();
+void setupIR() {
+  myReceiver.enableIRIn();
 }
 
+void setup() {
+  setupWarningLight();
+  setupLEDStrip();
+  setupRGBSensor();
+  setupIR();
+}
 
-color getColor() {
-  uint16_t clear, red, green, blue;
+// ******** Support Functions
+
+// Should something go wrong, provide a way to tell
+void indicateError() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  while (1); // halt!
+}
+
+void blinkLED(uint8_t delayLength) {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(delayLength);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void blinkLED(uint8_t delayLength, uint8_t times) {
+  blinkLED(delayLength);
+  if (times > 0) {
+    for(i = 1; i < times; ++i) {
+      delay(delayLength);
+      blinkLED(delayLength);
+    }
+  }
+}
+
+void setNextColorFromRemoteInput() {
+  for (i = 0; i < 20; ++i) {
+    if (remoteColors[i].key == currentInput) {
+      nextColor = remoteColors[i].color;
+      return;
+    }
+  }
+  nextColor = offColor;
+}
+
+void getNextColorFromSensor() {
 
   tcs.setInterrupt(false);      // turn on LED
-
   delay(60);  // takes 50ms to read
-
   tcs.getRawData(&red, &green, &blue, &clear);
-
   tcs.setInterrupt(true);  // turn off LED
 
-  //  Serial.print("C: "); Serial.print(clear);
-  //  Serial.print("\tR: "); Serial.print(red);
-  //  Serial.print("\tG: "); Serial.print(green);
-  //  Serial.print("\tB: "); Serial.print(blue);
-
   // Figure out some basic hex code for visualization
-  uint32_t sum = clear;
-  float r, g, b;
+  sum = clear;
+
   r = red; r /= sum;
   g = green; g /= sum;
   b = blue; b /= sum;
-  r *= brightness; g *= brightness; b *= brightness;
+  r *= 255; g *= 255; b *= 255;
 
-  color returnColor = { byte(r), byte(g), byte(b) };
-
-  return returnColor;
+  nextColor.red = byte(r);
+  nextColor.green = byte(g);
+  nextColor.blue = byte(b);
 }
 
-void shiftColorHistory() {
-  for (uint8_t i = 8; i > 0; i--) {
-    strip.setPixelColor(i, strip.getPixelColor(i - 1));
-  }
-}
-
-void pushColorHistory(uint32_t newColor) {
-  shiftColorHistory();
-  strip.setPixelColor(0, newColor);
-  strip.show();
-}
-
-void setStripColor(color current) {
+void setStripColor() {
+  offset = (float)brightness / 255.0;
   Driver.begin(); // begin
-  Driver.SetColor(current.red, current.green, current.blue);
+  Driver.SetColor(
+    (uint8_t)(currentColor.red * offset),
+    (uint8_t)(currentColor.green * offset),
+    (uint8_t)(currentColor.blue * offset)
+  );
   Driver.end();
 }
 
-int getMaxColorDifference(color oldColor, color newColor) {
-  return max(abs(oldColor.red - newColor.red), max(abs(oldColor.green - newColor.green), abs(oldColor.blue - newColor.blue)));
-}
-
-// Fade between colors. Fade time depends on largest R/G/B color difference
-void fadeStripColor(color oldColor, color newColor) {
-  float redDiff = newColor.red - oldColor.red;
-  float greenDiff = newColor.green - oldColor.green;
-  float blueDiff = newColor.blue - oldColor.blue;
-  byte i = 0;
-  byte len = getMaxColorDifference(oldColor, newColor);
-  float offset;
-  color tween = { 0, 0, 0 };
+void fadeStripColor() {
+  color_s oldColor = currentColor;
   
-  for (i = 1; i <= len; i++) {
-    offset = (float)i / (float)len;
-    tween.red = oldColor.red + (int)(redDiff * offset);
-    tween.green = oldColor.green + (int)(greenDiff * offset);
-    tween.blue = oldColor.blue + (int)(blueDiff * offset);
+  r1 = (nextColor.red - currentColor.red);
+  g1 = (nextColor.green - currentColor.green);
+  b1 = (nextColor.blue - currentColor.blue);
 
-    setStripColor(tween);
-    delay(20);
+  len = max(abs(r1), max(abs(g1), abs(b1)));
+
+  for (i = 1; i < len; ++i) {
+    currentColor.red = oldColor.red + (1.0 * r1 * i) / len;
+    currentColor.green = oldColor.green + (1.0 * g1 * i) / len;
+    currentColor.blue = oldColor.blue + (1.0 * b1 * i) / len;
+
+    setStripColor();
+    delay(5);
   }
+
+  currentColor = nextColor;
+  setStripColor();
 }
+
+void pushNewColor() {  
+  // Fade to new color
+  fadeStripColor();
+  // Update the data;
+}
+
+void getActionFromIR() {
+  if (currentInput == 0xFFFFFFFF) {
+    // Repeated key. Ignore for now
+    return;
+  }
+
+  if (!poweredOn && currentInput != 0xFF02FD) {
+    // Only accept Power Button if it is off
+    
+  } else if (currentInput == 0xFF827D) {
+    // Start/Stop listening to RGB sensor
+    // This lets us keep a current RGB setting
+    getSensorData = !getSensorData;
+    
+  } else if (currentInput == 0xFF02FD) {
+    // On/Off button
+    poweredOn = !poweredOn;
+    
+    if (poweredOn) {
+      nextColor = storedColor;
+      fadeStripColor();
+    } else {
+      storedColor = currentColor;
+      nextColor = offColor;
+      fadeStripColor();
+    }
+  } else if (currentInput == 0xFF3AC5) {
+    // Brighter
+    if (brightness == 0) {
+      brightness == 7;
+      setStripColor();
+    } else if (brightness <= 247) {
+      brightness += 8;
+      setStripColor();
+    }
+  } else if (currentInput == 0xFFBA45) {
+    // Darker
+    if (brightness >= 7) {
+      brightness -= 8;
+      setStripColor();
+    } else if (brightness != 0) {
+      brightness = 0;
+      setStripColor();
+    }
+  } else {
+    // No more special cases
+    setNextColorFromRemoteInput();
+
+    if (nextColor.red != 0 || nextColor.green != 0 || nextColor.blue != 0 ) {
+      getSensorData = false; // Stop checking for a new color
+      pushNewColor();
+    }
+  }
+
+  // Reset the currentInput so we don't reuse it.
+  currentInput = 0xFFFFFFFF;
+}
+
 
 void loop() {
-  // Delay up front. The RGB strip can start in a random state. If in a posistion to cause feedback, this should avoid it.
-  //
-  delay(5000);
+  // Delay up front. The RGB strip starts in a random state.
+  // If in a posistion to cause feedback, this should avoid it.
+  delay(200);
 
-  color newColor = getColor();
-  color colorGamma = { gammatable[newColor.red], gammatable[newColor.green], gammatable[newColor.blue] };
-
-  // Show gamma on history strip
-  pushColorHistory(strip.Color(colorGamma.red, colorGamma.green, colorGamma.blue));
-
-  // Fade to new color
-  fadeStripColor(stripColor, newColor);
-  // fadeStripColor(stripColor, colorGamma);
-
-  // Set color directly
-  // setStripColor(newColor);
-
-  stripColor = newColor;
-  //  stripColor = colorGamma;
-
+  if (myReceiver.getResults()) {
+        
+    myDecoder.decode();
+    currentInput = myDecoder.value; 
+    
+    blinkLED(20);
+    
+    getActionFromIR();
+    myReceiver.enableIRIn();      //Restart receiver
+    
+  } else if (poweredOn && getSensorData) {
+    getNextColorFromSensor();
+    pushNewColor();
+    //blinkLED(50);
+  }
 }
-
